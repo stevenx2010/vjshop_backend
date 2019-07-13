bc7280eac8199575860cb5aed6be17bc6616bae2
stevenx2010 <stevenx@yitongxun.cn>
2019-07-13 19:47:40 +0800

    add one file

---
+++ app/Libraries/Utilities/ProductProperty.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Libraries\Utilities;
+
+abstract class ProductProperty {
+	const BRAND_VJ = '稳卓';
+	const BRAND_HF = 'Hofmann';
+	const PACKAGE_BOX = '盒装';
+	const PACKAGE_PAN = '盘装';
+	const COATING_ZINC = '镀锌';
+	const COATING_COLOR = '彩色';
+	const QUALITY_AFTERMARKET = '售后';
+	const QUALITY_OEM = 'OEM原厂';
+}
